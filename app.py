c0ef3dd95fc738d2c80b5f2fef406ccc0167b985
Ray Chu <raytanaka8@gmail.com>
2024-10-31 15:04:25 -0700

    final

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, url_for, render_template, request, Blueprint, redirect, session
-
-app = Flask(__name__)
-
-@app.route("\main")
-def hello():
-    print("hello world")
-    return render_template("main.html")
