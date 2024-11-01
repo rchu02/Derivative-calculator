45f26cad081fe5a3c1b745ff7cf96d88b0f5310c
Ray Chu <raytanaka8@gmail.com>
2024-10-31 21:15:07 -0700

    README file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from latex2sympy2 import latex2latex
 from ast_to_latex import latex_equation, RIGHT, LEFT
 import sys
-import time
 
 def display_diff(eq):
     diff = r"\frac{d}{dx}"
@@ -18,19 +17,14 @@ def differentiation_solver(eq):
 if __name__ == "__main__":
     eq = input("Input your equation to differentiate: ")
     try:
-        start_time = time.time()
         diff = display_diff(eq)
         print(f"\nThis is your equation in LaTeX: {display_diff(eq)}\n")
-        print("--- %s seconds ---" % (time.time() - start_time))
     except:
         print("Invalid Equation")
         sys.exit(0)
     
     proc = input("Y/N to differentiate: ")
-    start_time = time.time()
     if proc == "Y" or proc == "y":
         print(f"\nYour Differentiated equation \n {differentiation_solver(diff)}")
     else:
         sys.exit(0)
-    start_time = time.time()
-print("--- %s seconds ---" % (time.time() - start_time))
